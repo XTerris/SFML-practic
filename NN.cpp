ccd65bf277c050e4b796159ecff23413d95d072f
Max <redunicorn94@gmail.com>
2021-02-23 20:24:42 +0300

    Added NN class

---
+++ NN.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include "NN.h"
+using namespace std;
+float NN::Activation(float x) {
+
+}
+float NN::Derivative(float x) {
+
+}
+NN::NN(vector<int> structure) {
+
+}
+vector<float> NN::FeedForward(vector<float> input) {
+
+}
---
+++ NN.h
@@ -0,0 +1,16 @@
+#include <vector>
+using namespace std;
+struct Neuron {
+	vector<float> input;
+	vector<float> weights;
+	float output;
+};
+class NN {
+private:
+	vector<vector<Neuron>> structure;
+	float Activation(float x);
+	float Derivative(float x);
+public:
+	NN(vector<int> structure);
+	vector<float> FeedForward(vector<float> input);
+};
